ec4eef4ecc7b040da2d8435f3869b00cdabfc630
Tri <trimpham98@gmail.com>
2023-10-28 15:05:49 -0700

    Place camera straight onto screen of webcam object

--- src/script.js
+++ src/script.js
@@ -113,8 +113,8 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.x = 1;
-camera.position.y = 1;
+camera.position.x = 0;
+camera.position.y = 0;
 camera.position.z = 2;
 scene.add(camera);
 
